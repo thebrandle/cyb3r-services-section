2c8686868bc1ec9773e01de21a094d368eab5535
cyb3r deploy <deploy@cyb3r.local>
2026-07-22 14:39:26 +0400

    home loader: pin sub-resources to immutable commit 7961148

--- home-loader.js
+++ home-loader.js
@@ -1,11 +1,13 @@
 /* CYB3R homepage sections loader (Selected Work .wk + Design in Motion .dm helix).
  * Loads three.js then the section CSS/JS from thebrandle/cyb3r-services-section@main.
  * Forked from muteebuilds/cyb3r-sections (that repo is not pushable by this account).
- * Sub-resources are pulled from @main so future edits are push + jsDelivr purge, no re-register. */
+ * Sub-resources are pinned to an immutable @<commit> (jsDelivr @main resolution was
+ * documented as unreliable/stale up to 12h). To update: push new js/css, bump B to the
+ * new commit, re-register this loader (bumped version), re-apply, publish. */
 (function () {
   if (!document.querySelector('.wk,.dm')) return;
   var d = document, h = d.head,
-      B = 'https://cdn.jsdelivr.net/gh/thebrandle/cyb3r-services-section@main/';
+      B = 'https://cdn.jsdelivr.net/gh/thebrandle/cyb3r-services-section@7961148ba8994c828e3d63f3ea1b0328127adb53/';
   var l = d.createElement('link'); l.rel = 'stylesheet'; l.href = B + 'cyb3r-sections.css'; h.appendChild(l);
   function j(u, c) { var s = d.createElement('script'); s.src = u; if (c) s.onload = c; h.appendChild(s); }
   j('https://cdnjs.cloudflare.com/ajax/libs/three.js/r128/three.min.js', function () { j(B + 'cyb3r-sections.js'); });
